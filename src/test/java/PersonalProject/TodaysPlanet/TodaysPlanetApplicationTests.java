d468a029e6a1550e59e398ac7fee5dc61f0938bf
halo <dongjin778@naver.com>
2025-07-11 13:41:09 +0900

    d

---
+++ src/test/java/PersonalProject/TodaysPlanet/TodaysPlanetApplicationTests.java
@@ -0,0 +1,13 @@
+package PersonalProject.TodaysPlanet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodaysPlanetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
